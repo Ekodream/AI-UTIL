90e06909a87a5a3059407e9ff89c49b048c00f64
Analysis <out2822676879@outlook.com>
2025-10-25 16:34:43 +0800

    YoungUstcUtil

---
+++ youngustc.py
@@ -0,0 +1,13 @@
+
+class YoungUstcUtil:
+    def __init__(self):
+        pass
+    
+    def screenshot(self):
+        pass
+
+    def nextpage():
+        pass
+
+    def call_model_generate_events(screenshot_path: str, output_file: str, model: str = 'qwen3-vl-plus', api_key: str | None = None, base_url: str | None = None, user_text: str | None = None) -> str:
+        pass
